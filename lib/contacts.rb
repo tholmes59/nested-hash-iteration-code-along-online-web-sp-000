7358a4318e42656b18c33974e136686b49a562b7
Tom Holmes <tholmes59@gmail.com>
2019-01-19 00:12:01 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,9 @@ require 'pry'
   
 def remove_strawberry(contacts)
 contacts.each do |person, contact_details_hash|
- binding.pry
+ contact_details_hash.each do |attribute, data|
+  if attribute === :favorite_ice_cream_flavors
+ end
  end
 end
  
